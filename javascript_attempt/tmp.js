b09d55f993bc6355df53871103d816e55fd2d421
Megan Van Welie <megan.van.welie@gmail.com>
2015-12-12 14:31:48 -0500

    drawing polygon in js

--- javascript_attempt/tmp.js
+++ javascript_attempt/tmp.js
@@ -1,8 +1,8 @@
 // Global and file-global constants
 NS ="http://www.w3.org/2000/svg";
 POINT_RAD = 10;
-
-var currentView = null;
+CANVAS_WIDTH = 1200;
+CANVAS_HEIGHT = 800;
 
 var Point = function(x, y, rad, background, highlight) {
     this.x = x;
@@ -21,76 +21,84 @@ var Edge = function(p1, p2, background, highlight) {
     this.highlight = highlight || "rgb(0, 0, 0)";
 }
 
-var PolygonCreationView = function(x, y, w, h) {
-    this.base = SVGView;
-    this.base(x, y, w, h);
-
+var PolygonCreationView = function(svg) {
+    this.svg = svg;
     this.points = new Array();
     this.edges = new Array();
 
     this.isComplete = false;
     this.tryPoint = true;
 
-    this.finishPolygon() {
+    this.finishPolygon = function(){
+        console.log("Finish polygon");
         this.isComplete = true;
         this.tryPoint = false;
-        addEdge(points[points.length - 1], points[0]);
+        this.addEdge(this.points[this.points.length - 1], this.points[0]);
+        return this;
     }
 
-    this.addEdge = new function(p1, p2){
-        edges.add(new Edge(p1, p2));
+    this.addEdge = function(p1, p2){
+        var newEdge = new Edge(p1, p2);
+        this.edges.push(newEdge);
+        var element = document.createElementNS(NS, "line");
+        element.setAttribute("x1", newEdge.x1);
+        element.setAttribute("y1", newEdge.y1);
+        element.setAttribute("x2", newEdge.x2);
+        element.setAttribute("y2", newEdge.y2);
+        element.setAttribute("stroke", newEdge.background);
+        this.svg.appendChild(element);
+        return this;
     }
 
-    this.addPoint = function(){
-        var newPoint = null;
-        if(!isComplete){
-            if(points.length > 0){
-                if(Math.abs(points[0].x - x <= 3) &&
-                    Math.abs(poitns[0].y - y <=3)){
-                    finishPolygon();
+    this.addPoint = function(x, y){
+        if(!this.isComplete){
+            var newPoint = null;
+            if(this.points.length > 0){
+                var xdiff = Math.abs(this.points[0].x - x);
+                var ydiff = Math.abs(this.points[0].y - y);
+                if(xdiff <= 5 && ydiff <= 5) {
+                    this.finishPolygon();
                 } else {
                     newPoint = new Point(x, y);
-                    addEdge(points[points.length - 2], newPoint);
+                    this.addEdge(
+                        this.points[this.points.length - 1], newPoint);
                 }
             } else{
                 newPoint = new Point(x, y);
             }
+            if(newPoint){
+                console.log("New point " + x + " " + y);
+                this.points.push(newPoint);
+                var element = document.createElementNS(NS, "circle");
+                element.setAttribute("cx", newPoint.x);
+                element.setAttribute("cy", newPoint.y);
+                element.setAttribute("fill", newPoint.background);
+                element.setAttribute("r", POINT_RAD);
+                this.svg.appendChild(element);
+            }
         }
-        if(newPoint){
-            points.add(newPoint);
-            var element = document.createElementNS(NS, "circle");
-            element.setAttribute("fill", newPoint.background);
-            element.setAttribute("r", POINT_RAD);
-            this.svg.appendChild(element);
-        }
+        return this;
     }
 
-    this.setCB = function(){
-        var clickCB = function() {
-            this.polygon.addPoint(event.clientX, event.clientY);
-        }
-        this.svg.addEventListener("click", clickCB.bind(this));
+    var clickCB = function() {
+        this.addPoint(event.clientX, event.clientY);
     }
-}
+    this.svg.addEventListener("click", clickCB.bind(this));
 
-var SVGView = function(x, y, w, h) {
-    this.x = x;
-    this.y = y;
-    this.w = w;
-    this.h = h;
-    this.svg = document.createElementNS(NS, "svg");
-    this.svg.setAttribute("width", w);
-    this.svg.setAttribute("height",h);
 }
 
 function init() {
-    var polyView = new PolygonCreationView(0, 0, 500, 500);
-    currentView = polyView;
+    console.log("INIT");
+    svg = document.createElementNS(NS, "svg");
+    svg.setAttribute("width", CANVAS_WIDTH);
+    svg.setAttribute("height", CANVAS_HEIGHT);
+    document.body.appendChild(svg);
+
+    var polyView = new PolygonCreationView(svg);
+
     window.requestAnimationFrame(draw);
 }
 
 function draw(){
-    if(currentView){
-        window.requestAnimationFrame(draw);
-    }
+    window.requestAnimationFrame(draw);
 }
